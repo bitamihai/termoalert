97153109fc9a6a123a0417a50a6582a00a4ec85a
Bita, Mihai-Valentin <mihai.valentin.bita@gmail.com>
2023-12-12 21:43:45 +0200

    Send GET request and parse HTML

--- src/main/java/mbita/Main.java
+++ src/main/java/mbita/Main.java
@@ -1,7 +1,45 @@
 package mbita;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    private static final long HTTP_CLIENT_CONNECT_TIMEOUT_SECONDS = 20;
+    public static final int HTTP_CLIENT_REQUEST_TIMEOUT_MINUTES = 2;
+    public static final String HTTP_CLIENT_URI = "https://www.cmteb.ro/functionare_sistem_termoficare.php";
+
+    public static void main(final String[] args) throws IOException, InterruptedException {
+        final HttpClient client = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_1_1)
+                .followRedirects(HttpClient.Redirect.NORMAL)
+                .connectTimeout(Duration.ofSeconds(HTTP_CLIENT_CONNECT_TIMEOUT_SECONDS))
+                .build();
+
+        final HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(HTTP_CLIENT_URI))
+                .timeout(Duration.ofMinutes(HTTP_CLIENT_REQUEST_TIMEOUT_MINUTES))
+                .GET()
+                .build();
+
+        final long timestampBeforeRequest = System.currentTimeMillis();
+        final HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        final long timestampAfterRequest = System.currentTimeMillis();
+
+        System.out.println(String.format("Request took %s millis", timestampAfterRequest - timestampBeforeRequest));
+        System.out.println(response.statusCode());
+        System.out.println(response.body());
+
+        final long timestampBeforeParse = System.currentTimeMillis();
+        final Document document = Jsoup.parse(response.body());
+        final long timestampAfterParse = System.currentTimeMillis();
+
+        System.out.println(String.format("Parse took %s millis", timestampAfterParse - timestampBeforeParse));
     }
 }
